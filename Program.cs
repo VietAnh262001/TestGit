e6a974a7ac02bee16a9a3273d87e1f767d008070
unknown <nguyenvietanh262001@gmail.com>
2021-03-12 14:27:27 +0700

    lan2

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ namespace TestGit
         static void Main(string[] args)
         {
             Console.WriteLine("chao cau chu");
+            Console.WriteLine("chao cau chu viet anh");
         }
     }
 }
